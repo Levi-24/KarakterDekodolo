043062ba8d6a8588a525ff15a4b5317c9a8ad6ff
Levente Major <leventemajor36@gmail.com>
2023-09-28 10:09:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Dekodolo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var karakterek = new List<Karakter>();
+            using StreamReader sr = new StreamReader(
+                path:@"..\..\..\src\bank.txt",
+                Encoding.UTF8
+                );
+
+            while (!sr.EndOfStream)
+            {
+                char b = char.Parse(sr.ReadLine());
+                bool[,] m = new bool[4, 7];
+                for (int s = 0; s < 4; s++)
+                {
+                    string sor = sr.ReadLine();
+                    for (int o = 0; o < sor.Length; o++)
+                    {
+                        m[s, o] = sor[o] == '1' ? true : false;
+                    }
+                }
+                karakterek.Add(new Karakter(b, m));
+            }
+        }
+    }
+}
