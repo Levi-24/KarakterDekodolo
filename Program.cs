16efaccfe2591ed427d3c3bbc5dd04adfa1ccb0f
Levente Major <leventemajor36@gmail.com>
2023-10-03 11:53:39 +0200

    sub

--- Program.cs
+++ Program.cs
@@ -10,9 +10,31 @@ namespace Dekodolo2
     {
         static void Main(string[] args)
         {
-            var bank = new List<Karakter>();
+            var bank = Beolvas(@"..\..\..\src\bank.txt");
+
+            Console.WriteLine("5.Feladat:");
+            Console.WriteLine($"Karakterek száma: {bank.Count}");
+
+            Console.WriteLine("6.Feladat:");
+            char input = '\0';
+            bool result = false;
+            do
+            {
+                Console.WriteLine("Karakter input: ");
+                result = char.TryParse(Console.ReadLine(), out input);
+            } while (!result || input < 65 || input > 90);
+
+            Console.WriteLine("7.Feladat:");
+            var megj = bank.SingleOrDefault(k => k.Betu == input);
+            if (megj is not null) Console.WriteLine(megj.Kirajzol());
+            else Console.WriteLine("Nincsen a karakter a bankban!");
+        }
+
+        static List<Karakter> Beolvas(string eleresiUt)
+        {
+            var karakterek = new List<Karakter>();
             using StreamReader sr = new StreamReader(
-                path:@"..\..\..\src\bank.txt",
+                eleresiUt,
                 Encoding.UTF8
                 );
 
@@ -28,24 +50,9 @@ namespace Dekodolo2
                         m[s, o] = sor[o] == '1';
                     }
                 }
-                bank.Add(new Karakter(b, m));
+                karakterek.Add(new Karakter(b, m));
             }
-
-            Console.WriteLine("5.Feladat:");
-            Console.WriteLine($"Karakterek száma: {bank.Count}");
-
-            Console.WriteLine("6.Feladat:");
-            char input = '\0';
-            bool result = false;
-            do
-            {
-                Console.WriteLine("Karakter input: ");
-                result = char.TryParse(Console.ReadLine(), out input);
-            } while (!result || input < 65 || input > 90);
-
-            var megj = bank.SingleOrDefault(k => k.Betu == input);
-            if (megj is not null) Console.WriteLine(megj.Kirajzol());
-            else Console.WriteLine("Nincsen a karakter a bankban!");
+            return karakterek;
         }
     }
 }
